a62ab7dd4f05733c96d3fc1a59358ff818490084
joshkat <joshua02k@gmail.com>
2022-12-11 10:58:49 -0500

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+let div = document.getElementsByClassName('mainContent')
+let templateHolder = document.getElementsByClassName('holder')
+let addTemplate = document.getElementsByTagName('button')
+
+var openMenuBtn = document.getElementsByClassName('template-button')[0],
+    menu        = document.getElementsByClassName('template-menu')[0];
+
+openMenuBtn.addEventListener('click', function ( e ) {
+    e.preventDefault();
+    menu.classList.toggle('opened');
+}, false);
+
+let chest = new Template('Chest', 'This is a workout for my chest!', premadeWorkouts, [])
+
+console.log(addTemplate)
+addTemplate[1].addEventListener('click', ()=>{
+    htmlTemplate(chest, templateHolder[0])
+})
+
+const d = new Date();
+let hour = d.getHours();
+const welcomeTag = document.getElementById('welcome')
+
+if(hour < 12 && hour >= 0){
+    welcomeTag.innerText = 'Good Morning!'
+}else if(hour >= 12 && hour < 17){
+    welcomeTag.innerText = 'Good Afternoon!'
+}else{
+    welcomeTag.innerText = 'Good Evening!' 
+}
+
+console.log(hour)
---
+++ scripts/workoutTemplate.js
@@ -0,0 +1,50 @@
+function Template(name, description, workouts, lastWorkout){
+    this.name = name //needed to create
+    this.description = description  //could be empty str to init
+    this.workouts = workouts    //empty arr to init
+    this.lastWorkout = lastWorkout //empty str to init
+}
+
+Template.prototype.addWorkoutToTemplate = function([name, notes, setInfo]){
+    this.workouts.push([name, notes, setInfo])
+}
+
+const premadeWorkouts = [
+    //order of each arr is name, notes, setInfo(left empty)
+    ['Bench Press', 'rn', 're'],
+    ['Incline Press', 're', 're']
+]
+
+let chestWorkout = new Template('Chest', '>.<', [])
+let backWorkout = new Template('Back', '', premadeWorkouts)
+
+chestWorkout.addWorkoutToTemplate(premadeWorkouts[0])
+console.log(chestWorkout, backWorkout)
+
+function htmlTemplate(objTemplate, /*userTemplateList,*/ HTMLRef){ //
+    var templateBorder = document.createElement('div'),
+        templateTopHead = document.createElement('div'),
+        templateTitle = document.createElement('h1'), //set innerText later
+        templateButton = document.createElement('button'), //innerTxt later
+        templateMenu = document.createElement('ol'),
+        templateWorkoutList = document.createElement('div'),
+        templateLastWorkout = document.createElement('div');
+
+    templateBorder.classList.add('template-border')    
+    templateTopHead.classList.add('template-topHead')
+    templateTitle.classList.add('template-title')
+    templateButton.classList.add('template-button')
+    templateMenu.classList.add('template-menu')
+    templateWorkoutList.classList.add('template-workOutList')
+    templateLastWorkout.classList.add('template-lastWorkout')
+
+    templateBorder.append(templateTopHead,templateWorkoutList, templateLastWorkout)//, templateWorkoutList, templateLastWorkout)
+    templateTopHead.append(templateTitle, templateButton, templateMenu)
+    templateTitle.innerText = objTemplate.name
+    templateButton.innerText = '•••'
+    templateWorkoutList.innerText = objTemplate.workouts
+    templateLastWorkout.innerText = objTemplate.lastWorkout
+
+    HTMLRef.append(templateBorder)
+    console.log('template added')
+}
