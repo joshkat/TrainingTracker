d8ae9afc7de700c7beb521c606be1420b97f0ab9
joshkat <joshua02k@gmail.com>
2023-01-04 17:33:10 -0500

    added proper login with info replacement

--- scripts/profile/firebaseLogin.js
+++ scripts/profile/firebaseLogin.js
@@ -1,3 +1,27 @@
+const mainContent = document.querySelector(".mainContent");
+
+let loginBoxParent = document.createElement("div");
+loginBoxParent.classList.add("buttonHolder");
+loginBoxParent.innerHTML = `
+    <span>Login using Google to get started!</span>
+    <button id="login-button">Login</button>
+`;
+
+let profileBoxParent = document.createElement("div");
+profileBoxParent.classList.add("profileHolder");
+profileBoxParent.innerHTML = `
+  <div class="profile-main">
+    <img
+      src="https://i.scdn.co/image/ab6775700000ee8522b26bdddf0ff534801e3756"
+      alt="profileIMG"
+      width="150px"
+    />
+    <span class="name">NAME</span>
+    <span>WORKOUT NUMBER</span>
+    <span>GOAL</span>
+  </div>
+`;
+
 //init firebase
 var firebaseConfig = {
   apiKey: "AIzaSyDWzPbgPFSjVgRIFCoNC4BTAkpW9s3GZnw",
@@ -11,7 +35,7 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 // select the login button
-const loginButton = document.getElementById("login-button");
+const loginButton = loginBoxParent.querySelector("button");
 
 loginButton.addEventListener("click", function () {
   // Initiate the login process with Google authentication
@@ -32,14 +56,21 @@ loginButton.addEventListener("click", function () {
     });
 });
 
+// const loginBox = document.getElementById("buttonHolder");
+const profileHolder = document.querySelector(".profile-main");
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
-    // The user is logged in, you can access their data here
+    // The user is logged in
+    profileBoxParent.querySelector(".name").textContent = user.displayName; //change username
+    profileBoxParent.querySelector("img").src = user.photoURL; //change img
+
+    mainContent.append(profileBoxParent); //append when logged in
+
     console.log("User ID:", user.uid);
     console.log("Email:", user.email);
     console.log("Display Name:", user.displayName);
     console.log("Profile Picture URL:", user.photoURL);
   } else {
-    // The user is not logged in
+    mainContent.append(loginBoxParent); //append when !logged in
   }
 });
