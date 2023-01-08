ae2048a8079556a2b02fc71945c97798704b691f
joshkat <joshua02k@gmail.com>
2023-01-08 17:22:57 -0500

    added db storage for measurment tab

--- scripts/measurements/firebaseMeasure.js
+++ scripts/measurements/firebaseMeasure.js
@@ -3,6 +3,7 @@ var firebaseConfig = {
   apiKey: "AIzaSyDWzPbgPFSjVgRIFCoNC4BTAkpW9s3GZnw",
   authDomain: "training-tracker-d8802.firebaseapp.com",
   projectId: "training-tracker-d8802",
+  databaseURL: "https://training-tracker-d8802-default-rtdb.firebaseio.com",
   storageBucket: "training-tracker-d8802.appspot.com",
   messagingSenderId: "154863339050",
   appId: "1:154863339050:web:5e4898dd89b44f80c26553",
@@ -10,13 +11,41 @@ var firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
+let userID, database;
 const profileHolder = document.querySelector(".profile-main");
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     // The user is logged in
+    userID = user.uid;
+    database = firebase.database();
     mainContent.innerHTML = "";
+    database
+      .ref(`users/${userID}/measurements/`)
+      .once("value")
+      .then(function (snapshot) {
+        setupWhenLoggedIn(snapshot.val());
+      });
     mainContent.append(info); //append info
   } else {
     window.location.replace("../index.html"); //redirect when !logged in
   }
 });
+
+function setupWhenLoggedIn(dbArr) {
+  //add DB event listeners
+  const inputArr = info.querySelectorAll("input");
+  console.log(dbArr[inputArr[3].id]);
+  for (var i = 0; i < inputArr.length; i++) {
+    if (dbArr[inputArr[i].id] != undefined) {
+      inputArr[i].value = dbArr[inputArr[i].id];
+    }
+    inputArr[i].addEventListener("input", (event) => {
+      if (event.target.value !== "") {
+        //when str is num
+        database
+          .ref(`users/${userID}/measurements/${event.target.id}`)
+          .set(event.target.value);
+      }
+    });
+  }
+}
--- scripts/measurements/measure.js
+++ scripts/measurements/measure.js
@@ -13,32 +13,25 @@ info.innerHTML = `
   </li>
 </ul>
 
-<p>Body Parts</p>
+<p>Body Parts (inches)</p>
 <ul>
-  <li>Neck (in) <input type="number" id="Neck"></li>
-  <li>Shoulders (in)<input type="number" id="Shoulders"></li>
-  <li>Chest (in)<input type="number" id="Chest"></li>
-  <li>Left Bicep (in)<input id="LeftBicep" type="number"></li>
-  <li>Right Bicep (in)<input id="RightBicep" type="number"></li>
-  <li>Left Forearm (in)<input id="LeftForearm" type="number"></li>
+  <li>Neck <input type="number" id="Neck"></li>
+  <li>Shoulders <input type="number" id="Shoulders"></li>
+  <li>Chest <input type="number" id="Chest"></li>
+  <li>Left Bicep <input id="LeftBicep" type="number"></li>
+  <li>Right Bicep <input id="RightBicep" type="number"></li>
+  <li>Left Forearm <input id="LeftForearm" type="number"></li>
   <li>
-    Right Forearm (in)
+    Right Forearm 
     <input id="RightForearm" type="number">
   </li>
-  <li>Upper Abs (in)<input id="UpperAbs" type="number"></li>
-  <li>Waist (in)<input id="Waist" type="number"></li>
-  <li>Lower Abs (in)<input id="LowerAbs" type="number"></li>
-  <li>Hips (in)<input id="Hips" type="number"></li>
-  <li>Left Thigh (in)<input id="LeftThigh" type="number"></li>
-  <li>Right Thigh (in)<input id="RightThigh" type="number"></li>
-  <li>Left Calf (in)<input id="LeftCalf" type="number"></li>
-  <li>Right Calf (in)<input id="RightCalf" type="number"></li>
+  <li>Upper Abs <input id="UpperAbs" type="number"></li>
+  <li>Waist <input id="Waist" type="number"></li>
+  <li>Lower Abs <input id="LowerAbs" type="number"></li>
+  <li>Hips <input id="Hips" type="number"></li>
+  <li>Left Thigh <input id="LeftThigh" type="number"></li>
+  <li>Right Thigh <input id="RightThigh" type="number"></li>
+  <li>Left Calf <input id="LeftCalf" type="number"></li>
+  <li>Right Calf <input id="RightCalf" type="number"></li>
 </ul>
 `;
-
-const inputArr = info.querySelectorAll("input");
-for (var i = 0; i < inputArr.length; i++) {
-  inputArr[i].addEventListener("input", (event) => {
-    console.log(event.target.id);
-  });
-}
